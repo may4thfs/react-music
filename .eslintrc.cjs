6f1eaa9557131c852cbfd4f8c6a223c7e3aedff1
Feng, Shun <723278075@qq.com>
2024-03-06 19:19:12 +0800

    chore: 添加 eslint-plugin-react 并更新 eslint 配置
    - 将 eslint-plugin-react 添加到 devDependencies
    - 更新 .eslintrc，包含 'plugin:react/recommended' 和 'plugin:react/jsx-runtime'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
