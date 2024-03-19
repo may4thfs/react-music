d2709ad6f02a0dc654dc7808e86a0c39bb7f5d5c
Feng, Shun <723278075@qq.com>
2024-03-19 18:53:48 +0800

    style: 增加 css 类和精灵图

---
+++ src/assets/theme/index.ts
@@ -0,0 +1,17 @@
+// styled-components 管理全局变量样式
+const theme = {
+  color: {
+    primary: '#C20C0C',
+    secondary: ''
+  },
+  size: {},
+  // 混入
+  mixin: {
+    wrapv1: `
+      width: 1100px;
+      margin: 0 auto;
+    `
+  }
+}
+
+export default theme
