b0fb111d97489542a1c083c02603a35e10569dc9
Feng, Shun <723278075@qq.com>
2024-03-20 20:08:15 +0800

    feat: 在发现页面内增加顶部导航栏

---
+++ src/views/discover/c-cpns/nav-bar/index.tsx
@@ -0,0 +1,29 @@
+import { memo } from 'react'
+import type { FC, ReactNode } from 'react'
+import { NavLink } from 'react-router-dom'
+import { NavWrapper } from './style'
+import { discoverMenu } from '@/assets/data/local_data'
+
+interface IProps {
+  children?: ReactNode
+}
+
+const NavBar: FC<IProps> = memo(() => {
+  return (
+    <NavWrapper>
+      <div className="nav wrap-v1">
+        {discoverMenu.map((item) => {
+          return (
+            <div className="item" key={item.link}>
+              <NavLink to={item.link}>{item.title}</NavLink>
+            </div>
+          )
+        })}
+      </div>
+    </NavWrapper>
+  )
+})
+
+NavBar.displayName = 'NavBar'
+
+export default NavBar
